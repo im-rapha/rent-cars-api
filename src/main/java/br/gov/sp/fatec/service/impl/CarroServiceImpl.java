b7a0729807dda8d61fd3228215bad4267cef9a0c
Raphael Carvalho <104576666+im-rapha@users.noreply.github.com>
2024-07-02 19:57:13 -0300

    Update CarroServiceImpl.java

--- src/main/java/br/gov/sp/fatec/service/impl/CarroServiceImpl.java
+++ src/main/java/br/gov/sp/fatec/service/impl/CarroServiceImpl.java
@@ -1,52 +1,57 @@
 package br.gov.sp.fatec.service.impl;
 
-import br.gov.sp.fatec.domain.entity.Cliente;
-import br.gov.sp.fatec.domain.mapper.ClienteMapper;
-import br.gov.sp.fatec.domain.request.ClienteRequest;
-import br.gov.sp.fatec.domain.request.ClienteUpdateRequest;
-import br.gov.sp.fatec.domain.response.ClienteResponse;
-import br.gov.sp.fatec.repository.ClienteRepository;
-import br.gov.sp.fatec.service.ClienteService;
+import br.gov.sp.fatec.domain.entity.Carro;
+import br.gov.sp.fatec.domain.enums.CarroStatus;
+import br.gov.sp.fatec.domain.mapper.CarroMapper;
+import br.gov.sp.fatec.domain.request.CarroRequest;
+import br.gov.sp.fatec.domain.request.CarroUpdateRequest;
+import br.gov.sp.fatec.domain.response.CarroResponse;
+import br.gov.sp.fatec.repository.CarroRepository;
+import br.gov.sp.fatec.service.CarroService;
+import jakarta.persistence.EntityNotFoundException;
+
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class ClienteServiceImpl implements ClienteService {
-
-    private final ClienteRepository clienteRepository;
-    private final ClienteMapper clienteMapper;
+public class CarroServiceImpl implements CarroService {
+    private final CarroRepository carroRepository;
+    private final CarroMapper carroMapper;
 
     @Override
-    public ClienteResponse save(ClienteRequest clienteRequest) {
-        Cliente clienteSaved = clienteRepository.save(clienteMapper.map(clienteRequest));
-        return clienteMapper.map(clienteSaved);
+    public CarroResponse save(CarroRequest carroRequest) {
+        Carro carroSaved = carroRepository.save(carroMapper.map(carroRequest));
+        // Por padrão, cria com o status disponível
+        carroSaved.setStatus(CarroStatus.DISPONIVEL);
+        
+        return carroMapper.map(carroSaved);
     }
 
     @Override
-    public ClienteResponse findById(Long id) {
-        Cliente cliente = clienteRepository.findById(id).orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
-        return new ClienteResponse(cliente.getNome(), cliente.getCpf(), cliente.getTelefone());
+    public CarroResponse findById(Long id) {
+        Carro carro = carroRepository.findById(id).orElseThrow(() -> new RuntimeException("Carro não encontrado"));
+        return new CarroResponse(carro.getAno(), carro.getMarca(), carro.getModelo());
     }
 
     @Override
-    public List<ClienteResponse> findAll() {
-        List<Cliente> clientes = clienteRepository.findAll();
-        return clientes.stream().map(clienteMapper::map).toList();
+    public List<CarroResponse> findAll() {
+        List<Carro> carros = carroRepository.findAll();
+        return carros.stream().map(carroMapper::map).toList();
     }
 
     @Override
-    public void updateById(Long id, ClienteUpdateRequest clienteUpdateRequest) {
-        Cliente cliente = clienteRepository.findById(id).orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
-        cliente.setNome(clienteUpdateRequest.nome());
-        cliente.setCpf(clienteUpdateRequest.cpf());
-        cliente.setTelefone(clienteUpdateRequest.telefone());
-        clienteRepository.save(cliente);
+    public void updateById(Long id, CarroUpdateRequest carroUpdateRequest) {
+        Carro carro = carroRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Carro não encontrado"));
+        carro.setAno(carroUpdateRequest.ano());
+        carro.setMarca(carroUpdateRequest.marca());
+        carro.setModelo(carroUpdateRequest.modelo());
+        carroRepository.save(carro);
     }
 
     @Override
     public void deleteById(Long id) {
-        clienteRepository.deleteById(id);
+        carroRepository.deleteById(id);
     }
 }
