57bf7e1291feb0641ec7af683ca94707ba21082c
Raphael Carvalho <104576666+im-rapha@users.noreply.github.com>
2024-07-02 19:56:02 -0300

    Update CarroServiceImpl.java

--- src/main/java/br/gov/sp/fatec/service/impl/CarroServiceImpl.java
+++ src/main/java/br/gov/sp/fatec/service/impl/CarroServiceImpl.java
@@ -1,40 +1,52 @@
 package br.gov.sp.fatec.service.impl;
 
-import br.gov.sp.fatec.domain.mapper.CarroMapper;
-import br.gov.sp.fatec.domain.request.CarroRequest;
-import br.gov.sp.fatec.domain.request.CarroUpdateRequest;
-import br.gov.sp.fatec.domain.response.CarroResponse;
-import br.gov.sp.fatec.repository.CarroRepository;
-import br.gov.sp.fatec.service.CarroService;
+import br.gov.sp.fatec.domain.entity.Cliente;
+import br.gov.sp.fatec.domain.mapper.ClienteMapper;
+import br.gov.sp.fatec.domain.request.ClienteRequest;
+import br.gov.sp.fatec.domain.request.ClienteUpdateRequest;
+import br.gov.sp.fatec.domain.response.ClienteResponse;
+import br.gov.sp.fatec.repository.ClienteRepository;
+import br.gov.sp.fatec.service.ClienteService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CarroServiceImpl implements CarroService {
+public class ClienteServiceImpl implements ClienteService {
 
-    private final CarroRepository carroRepository;
-    private final CarroMapper carroMapper;
+    private final ClienteRepository clienteRepository;
+    private final ClienteMapper clienteMapper;
 
     @Override
-    public CarroResponse save(CarroRequest carroRequest) {
-        return null;
+    public ClienteResponse save(ClienteRequest clienteRequest) {
+        Cliente clienteSaved = clienteRepository.save(clienteMapper.map(clienteRequest));
+        return clienteMapper.map(clienteSaved);
     }
 
     @Override
-    public CarroResponse findById(Long id) {
-        return null;
+    public ClienteResponse findById(Long id) {
+        Cliente cliente = clienteRepository.findById(id).orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+        return new ClienteResponse(cliente.getNome(), cliente.getCpf(), cliente.getTelefone());
     }
 
     @Override
-    public List<CarroResponse> findAll() {
-        return List.of();
+    public List<ClienteResponse> findAll() {
+        List<Cliente> clientes = clienteRepository.findAll();
+        return clientes.stream().map(clienteMapper::map).toList();
     }
 
     @Override
-    public void updateById(Long id, CarroUpdateRequest carroUpdateRequest) {}
+    public void updateById(Long id, ClienteUpdateRequest clienteUpdateRequest) {
+        Cliente cliente = clienteRepository.findById(id).orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+        cliente.setNome(clienteUpdateRequest.nome());
+        cliente.setCpf(clienteUpdateRequest.cpf());
+        cliente.setTelefone(clienteUpdateRequest.telefone());
+        clienteRepository.save(cliente);
+    }
 
     @Override
-    public void deleteById(Long id) {}
+    public void deleteById(Long id) {
+        clienteRepository.deleteById(id);
+    }
 }
